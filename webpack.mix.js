c2be0e5f4a986b0bbd6a2920f674e14b499d6b92
Doug Black Jr <dougblackjr@gmail.com>
2022-03-17 07:28:27 -0400

    Add scss and postcss compilation

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,8 @@
 const mix = require('laravel-mix')
 
 mix.sass('scss/main.scss', 'css')
+
+// If you are using PostCSS, comment out line 3, and uncomment line 6-8
+// mix.postCss('postcss/main.css', 'css', [
+//         require('postcss-import')
+//     ])
